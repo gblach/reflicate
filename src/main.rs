9140582d500d5f4a208420a7ac366c1fd06e4ce4
Grzegorz Blach <grzegorz@blach.pl>
2023-01-15 13:10:16 +0100

    Always ends directory path with '/'

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,8 @@ fn main() -> ExitCode {
 	let mut saved_bytes: u64 = 0;
 
 	for directory in args.directories.iter() {
-		let directory = Path::new(directory);
+		let directory = format!("{}/", directory);
+		let directory = Path::new(&directory);
 		let mut index: index::Index = HashMap::new();
 		let mut indexfile: index::IndexFile = HashMap::new();
 
